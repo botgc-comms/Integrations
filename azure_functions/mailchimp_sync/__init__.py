8a2fb5723e4d1dc0b567df96289923b764555ad4
Simon Parsons <simon@mmaraboustork.co.uk>
2024-07-08 14:51:03 +0100

    remove test function

--- azure_functions/mailchimp_sync/__init__.py
+++ azure_functions/mailchimp_sync/__init__.py
@@ -311,7 +311,7 @@ def update_mailchimp_async(merge_fields_collection):
     return added_count, updated_count
 
 
-def main(req: HttpRequest) -> HttpResponse:
+def main(req: HttpRequest = None, mytimer: TimerRequest = None) -> HttpResponse:
     logging.info("Azure function 'mailchimp_sync' triggered.")
     if member_login() and obtain_admin_rights():
         soup = execute_report()
